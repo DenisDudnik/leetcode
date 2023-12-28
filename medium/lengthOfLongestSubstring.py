2711a55990c28c22849445990b267efc34357779
Denis Dudnik <Denis.Dudnik@gmail.com>
2023-12-28 08:34:10 +0000

    2023-12-28

--- medium/lengthOfLongestSubstring.py
+++ medium/lengthOfLongestSubstring.py
@@ -1,24 +1,39 @@
 # https://leetcode.com/problems/longest-substring-without-repeating-characters/
 
-# Given a string s, find the length of the longest substring without repeating characters.
-# Constraints:
-
-# 0 <= s.length <= 5 * 104
-# s consists of English letters, digits, symbols and spaces.
 
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
+        # if len(s) < 2:
+        #     return len(s)
+        # max_len = 0
+        # simbols = []
+        # for simbol in s:
+        #     if simbols and simbol in simbols:
+        #         max_len = max(max_len, len(simbols))
+        #         simbols = simbols[simbols.index(simbol):]
+        #         simbols.pop(0)
+        #     simbols.append(simbol)
+        # max_len = max(max_len, len(simbols))
+        # return max_len
+
         if len(s) < 2:
             return len(s)
-        max_len = 0
-        simbols = []
-        for simbol in s:
-            if simbols and simbol in simbols:
-                max_len = max(max_len, len(simbols))
-                simbols = simbols[simbols.index(simbol):]
-                simbols.pop(0)
-            simbols.append(simbol)
-        max_len = max(max_len, len(simbols))
+
+        simbols = {}
+        left_idx, max_len = 0, 0
+        for right_idx, simbol in enumerate(s):
+            if simbol not in simbols:
+                simbols[simbol] = right_idx
+            else:
+                if simbols[simbol] >= left_idx:
+                    left_idx = simbols[simbol] + 1
+                simbols[simbol] = right_idx
+
+            max_len = (
+                max_len
+                if max_len > right_idx - left_idx + 1
+                else right_idx - left_idx + 1
+            )
         return max_len
 
 
@@ -28,3 +43,4 @@ if __name__ == "__main__":
     assert Solution().lengthOfLongestSubstring("pwwkew") == 3
     assert Solution().lengthOfLongestSubstring("a") == 1
     assert Solution().lengthOfLongestSubstring("au") == 2
+    assert Solution().lengthOfLongestSubstring("abba") == 2
