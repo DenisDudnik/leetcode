91da43f873fa6650f6c3423be82602592e69e63b
Denis Dudnik <Denis.Dudnik@gmail.com>
2024-01-03 09:22:53 +0000

    2024-01-03

--- easy/twoSum.py
+++ easy/twoSum.py
@@ -1,3 +1,5 @@
+# https://leetcode.com/problems/two-sum/
+
 from typing import List
 
 
@@ -6,6 +8,12 @@ class Solution:
         nums_dict = {}
         for k, v in enumerate(nums):
             first = target - v
-            if first in nums_dict.keys():
+            if first in nums_dict:
                 return [k, nums_dict[first]]
             nums_dict[v] = k
+
+
+if __name__ == "__main__":
+    assert sorted(Solution().twoSum(nums=[2, 7, 11, 15], target=9)) == sorted([0, 1])
+    assert sorted(Solution().twoSum(nums=[3, 2, 4], target=6)) == sorted([1, 2])
+    assert sorted(Solution().twoSum(nums=[3, 3], target=6)) == sorted([0, 1])
