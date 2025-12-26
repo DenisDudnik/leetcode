56f0afe0428d11aeb0ba6d4681cd01488041de8c
Denis Dudnik <Denis.Dudnik@gmail.com>
2025-12-26 11:34:19 +0300

    2025-12-26

--- hard/minWindow.py
+++ hard/minWindow.py
@@ -1,36 +1,33 @@
 # https://leetcode.com/problems/minimum-window-substring/
 
 
+# 2025-12-26
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        t_dict, win_dict, left, right, best, sub, found = {}, {}, 0, 0, None, "", 0
-        for c in t:
-            t_dict[c] = t_dict.get(c, 0) + 1
-        for idx, c in enumerate(s):
-            right = idx
-            if c in t_dict:
-                win_dict[c] = win_dict.get(c, 0) + 1
-                if t_dict[c] >= win_dict[c]:
-                    found += 1
-            if found == len(t):
-                sub = s[left : right + 1]
-                for sub_c in sub:
-                    if sub_c not in win_dict:
-                        left += 1
-                    elif t_dict[sub_c] < win_dict[sub_c]:
-                        left += 1
-                        win_dict[sub_c] -= 1
-                    else:
-                        break
-                if not best:
-                    best = s[left : right + 1]
-                elif len(best) > right - left + 1:
-                    best = s[left : right + 1]
-                if s[left] in win_dict:
-                    win_dict[s[left]] -= 1
-                    found -= 1
+        from collections import Counter
+
+        need = Counter(t)
+        missing = len(t)
+
+        min_len = float("inf")
+        left = start = 0
+
+        for right, ch in enumerate(s):
+            if need[ch] > 0:
+                missing -= 1
+            need[ch] -= 1
+
+            while missing == 0:
+                if right - left + 1 < min_len:
+                    min_len = right - left + 1
+                    start = left
+
+                need[s[left]] += 1
+                if need[s[left]] > 0:
+                    missing += 1
                 left += 1
-        return best or ""
+
+        return "" if min_len == float("inf") else s[start : start + min_len]
 
 
 if __name__ == "__main__":
