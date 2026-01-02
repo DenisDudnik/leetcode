fd419dbb5263373a5188e3e265d98833a1fccab0
Denis Dudnik <Denis.Dudnik@gmail.com>
2026-01-02 10:17:57 +0300

    2026-01-02

--- medium/insert.py
+++ medium/insert.py
@@ -3,23 +3,23 @@
 from typing import List
 
 
-# 2025-12-26
+# 2026-01-02
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
         result = []
-        i, n = 0, len(intervals)
+        i, n = 0, len(intervals) - 1
 
-        while i < n and intervals[i][1] < newInterval[0]:
+        while i <= n and intervals[i][1] < newInterval[0]:
             result.append(intervals[i])
             i += 1
 
-        while i < n and intervals[i][0] <= newInterval[1]:
+        while i <= n and intervals[i][0] <= newInterval[1]:
             newInterval[0] = min(newInterval[0], intervals[i][0])
             newInterval[1] = max(newInterval[1], intervals[i][1])
             i += 1
         result.append(newInterval)
 
-        while i < n:
+        while i <= n:
             result.append(intervals[i])
             i += 1
 
