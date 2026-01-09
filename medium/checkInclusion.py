b19fab108447fa6acf5c2c4c934c31afb8f13135
Denis Dudnik <Denis.Dudnik@gmail.com>
2026-01-09 09:21:13 +0300

    2026-01-08

--- medium/checkInclusion.py
+++ medium/checkInclusion.py
@@ -16,10 +16,10 @@ class Solution:
         for i in range(len(s1), len(s2)):
             if d1 == d2:
                 return True
-            d2[s2[i]] = d2.get(s2[i], 0) + 1
             d2[s2[i - len(s1)]] -= 1
             if d2[s2[i - len(s1)]] == 0:
                 del d2[s2[i - len(s1)]]
+            d2[s2[i]] = d2.get(s2[i], 0) + 1
 
         return d1 == d2
 
---
+++ medium/findPeakElement.py
@@ -0,0 +1,26 @@
+# https://leetcode.com/problems/find-peak-element/
+
+from typing import List
+
+
+# 2026-01-08
+class Solution:
+    def findPeakElement(self, nums: List[int]) -> int:
+        left, right = 0, len(nums) - 1
+
+        while left < right:
+            mid = (left + right) // 2
+            if nums[mid] < nums[mid + 1]:
+                left = mid + 1
+            else:
+                right = mid
+        return left
+
+
+# tests
+s = Solution()
+assert s.findPeakElement([1, 2, 3, 1]) in [2]
+assert s.findPeakElement([1, 2, 1, 3, 5, 6, 4]) in [1, 5]
+assert s.findPeakElement([1]) in [0]
+assert s.findPeakElement([2, 1]) in [0]
+assert s.findPeakElement([1, 2]) in [1]
