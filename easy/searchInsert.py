a0f1b2341b780b32c0e2e951f854f7e4f8876317
Denis Dudnik <Denis.Dudnik@gmail.com>
2026-03-18 14:04:41 +0300

    2026-03-18

--- easy/searchInsert.py
+++ easy/searchInsert.py
@@ -4,18 +4,19 @@
 from typing import List
 
 
-# 2026-02-06
+# 2026-03-18
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
         left, right = 0, len(nums) - 1
         while left <= right:
             mid = (left + right) // 2
-            if target < nums[mid]:
-                right = mid - 1
-            elif target > nums[mid]:
-                left = mid + 1
-            else:
+            if nums[mid] == target:
                 return mid
+            elif target < nums[mid]:
+                right = mid - 1
+            else:
+                left = mid + 1
+
         return left
 
 
