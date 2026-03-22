ee571854ff0764eff1a05afa27af6a38c24df3f1
Denis Dudnik <Denis.Dudnik@gmail.com>
2026-03-22 09:29:56 +0300

    2026-03-22

--- medium/threeSum.py
+++ medium/threeSum.py
@@ -3,33 +3,30 @@
 from typing import List
 
 
-# 2026-02-23
+# 2026-03-22
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         nums.sort()
         res = []
 
         for i in range(len(nums)):
-            if i > 0 and nums[i] == nums[i - 1]:
-                continue
             if nums[i] > 0:
                 break
-
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
             target = -nums[i]
             left, right = i + 1, len(nums) - 1
-
             while left < right:
-                if nums[left] + nums[right] == target:
+                s = nums[left] + nums[right]
+                if s == target:
                     res.append([nums[i], nums[left], nums[right]])
-
                     while left < right and nums[left] == nums[left + 1]:
                         left += 1
                     while left < right and nums[right] == nums[right - 1]:
                         right -= 1
-
                     left += 1
                     right -= 1
-                elif nums[left] + nums[right] < target:
+                elif s < target:
                     left += 1
                 else:
                     right -= 1
