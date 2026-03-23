7846be0165a161191e966492d927505204d74e1d
Denis Dudnik <Denis.Dudnik@gmail.com>
2026-03-23 09:30:48 +0300

    2026-03-23

--- medium/characterReplacement.py
+++ medium/characterReplacement.py
@@ -1,21 +1,24 @@
 # https://leetcode.com/problems/longest-repeating-character-replacement/
 
-# 2026-03-09
+# 2026-03-23
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        left = 0
-        mostf = 0
-        counts = {}
+        left = longest = 0
+        most_f = 0
+        ch_freqs = {}
 
         for right, ch in enumerate(s):
-            counts[ch] = counts.get(ch, 0) + 1
-            mostf = max(mostf, counts[ch])
+            ch_freqs[ch] = ch_freqs.get(ch, 0) + 1
+            most_f = max(most_f, ch_freqs[ch])
 
-            while right - left + 1 - mostf > k:
-                counts[s[left]] -= 1
+            while right - left + 1 - most_f > k:
+                ch_freqs[s[left]] -= 1
                 left += 1
 
-        return len(s) - left
+            if right - left + 1 > longest:
+                longest = right - left + 1
+
+        return longest
 
 
 # tests
